a3320f0d8b2acb8ddcd0565a78cab3e9ad4f6549
Dima Toropov <toropov0014@mail.ru>
2017-05-26 16:11:36 +0300

    редактирование Vspomny_vse

--- Vspomny_vse.py
+++ Vspomny_vse.py
@@ -1,10 +1,10 @@
 # coding=utf-8
 # Декораторы - это просто функции
 def my_decorator(func):
-    print "Я обычная функция"
+    print "Я обычная  функция"
 
     def wrapper():
-        print "Я - функция, возвращаемая декоратором"
+        print "Я - функция, возвращаемая декоратором сюда добавляется функционал"
         func()
 
     return wrapper
@@ -13,20 +13,21 @@ def my_decorator(func):
 # Так что, мы можем вызывать её, не используя "@"-синтаксис:
 
 def lazy_function():
-    print "zzzzzzzz"
+    print "я функция которая декорируется zzzzzzzz"
 
 
-decorated_function = my_decorator(lazy_function)
-
-
-# выведет: Я обычная функция
-
+decorated_function = my_decorator(lazy_function) # то есть так передается уже готовая не редактируемая функция
+# выведет первый раз: Я обычная функция
 # Данный код выводит "Я обычная функция", потому что это ровно то, что мы сделали:
 # вызвали функцию. Ничего сверхъестественного
 
+print '001'
 @my_decorator
-def lazy_function():
-    print "zzzzzzzz"
+def lazy_function():   # получается мы здесь переопределяем старую функцию - lazy_function()
+    print "А я nnnnnnn"
+# выведет второй раз: Я обычная функция
+print '002'
 
-# выведет: Я обычная функция
 decorated_function()
+print '003'
+lazy_function()
